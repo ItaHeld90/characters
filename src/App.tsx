4093f86114752ba1ad91ea8cd4d64aa76732f830
Itamar Held <ita@gmail.com>
2020-08-02 18:43:26 +0300

    show multiple random drawings

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,63 @@
 import React, { useState } from 'react';
-import './App.css';
+import { times } from 'lodash';
 import { getRandomDrawing, drawingToText } from './drawing-utils';
+import './App.css';
 
 const charSet = ['+', '=', 'X', 'Y', 'o', 'O', 'H', '^', '.', '*', '8', '|'];
 const numRows = 10;
 const wingLen = 4;
 
+const numDrawings = 6;
+const numTileRows = 2;
+const numCols = Math.floor(numDrawings / numTileRows);
+
 function App() {
-    const [drawing] = useState(
-        getRandomDrawing(charSet, numRows, wingLen, { pSpace: 0.65 })
+    const [drawings] = useState(
+        times(numDrawings, () =>
+            getRandomDrawing(charSet, numRows, wingLen, { pSpace: 0.65 })
+        )
     );
-    const str = drawingToText(drawing, wingLen);
+
+    const strings = drawings.map((drawing) => drawingToText(drawing, wingLen));
 
     return (
         <div style={{ height: '100vh', fontSize: 20 }} className="App">
             <div
                 style={{
                     display: 'flex',
-                    alignItems: 'center',
-                    justifyContent: 'center',
+                    // alignItems: 'center',
+                    // justifyContent: 'center',
+                    flexDirection: 'column',
                     height: '100%',
                 }}
             >
-                <pre>{str}</pre>
+                {times(numTileRows, (rowIdx) => (
+                    <div key={rowIdx} style={{ display: 'flex', flex: 1 }}>
+                        {times(numCols, (colIdx) => (
+                            <div
+                                key={colIdx} // TODO: use id
+                                style={{
+                                    display: 'flex',
+                                    flex: 1,
+                                    alignItems: 'center',
+                                    justifyContent: 'center',
+                                }}
+                            >
+                                <div
+                                    style={{
+                                        padding: '20px 100px',
+                                        background: 'lightgrey',
+                                        cursor: 'pointer'
+                                    }}
+                                >
+                                    <pre>
+                                        {strings[colIdx + rowIdx * numCols]}
+                                    </pre>
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+                ))}
             </div>
         </div>
     );
