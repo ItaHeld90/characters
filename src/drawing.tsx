96b4364e42d9f462758fc61a9e503e367f70b3bc
Itamar Held <ita@gmail.com>
2020-08-06 01:17:38 +0300

    copy drawing to clipboard

--- src/drawing.tsx
+++ src/drawing.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import './drawing.css';
+
 interface DrawingProps {
     drawingText: string;
     isPicked: boolean;
@@ -7,6 +9,11 @@ interface DrawingProps {
 }
 
 export function Drawing({ isPicked, onPick, drawingText }: DrawingProps) {
+    function handleCopy(e: React.MouseEvent) {
+        e.stopPropagation();
+        return navigator.clipboard.writeText(drawingText);
+    }
+
     return (
         <div
             style={{
@@ -17,14 +24,29 @@ export function Drawing({ isPicked, onPick, drawingText }: DrawingProps) {
             }}
         >
             <div
+                className="drawing-container"
                 style={{
                     padding: '20px 100px',
                     background: isPicked ? 'lightgreen' : 'lightgrey',
                     cursor: 'pointer',
+                    position: 'relative',
                 }}
                 onClick={onPick}
             >
                 <pre>{drawingText}</pre>
+                <div
+                    className="drawing-actions-bar"
+                    style={{
+                        position: 'absolute',
+                        padding: 5,
+                        bottom: 0,
+                        left: 0,
+                    }}
+                >
+                    <button style={{ cursor: 'pointer' }} onClick={handleCopy}>
+                        copy
+                    </button>
+                </div>
             </div>
         </div>
     );
