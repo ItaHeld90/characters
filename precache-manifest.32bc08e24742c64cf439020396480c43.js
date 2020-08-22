513d1f6201ccacb195606a1d62fda6341e30f54c
Itamar Held <ita@gmail.com>
2020-08-22 10:14:14 +0300

    Updates

---
+++ precache-manifest.32bc08e24742c64cf439020396480c43.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9a4ffac9f0ac225387481710f753c29",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "2fefaa632370178048d9",
+    "url": "/my-app/static/css/main.c3523fa7.chunk.css"
+  },
+  {
+    "revision": "f779731305b44d98bb73",
+    "url": "/my-app/static/js/2.2766d3f1.chunk.js"
+  },
+  {
+    "revision": "2ffa4e2d3489409d5c218d5f7292033c",
+    "url": "/my-app/static/js/2.2766d3f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2fefaa632370178048d9",
+    "url": "/my-app/static/js/main.193c3144.chunk.js"
+  },
+  {
+    "revision": "3cbca6e585ff8240aa89",
+    "url": "/my-app/static/js/runtime-main.aa77fe18.js"
+  }
+]);
